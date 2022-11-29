ec694923503de5e3346b9926ec5a29dc5727014f
AChubatova <n.chubatova@gmail.com>
2022-11-28 22:24:14 -0800

    checkAssume

--- PrimeServiceTests/generateTestsAssume100.ps1
+++ PrimeServiceTests/generateTestsAssume100.ps1
@@ -1,4 +1,4 @@
-for (($i = 0); $i -lt 2000; $i++)
+for (($i = 0); $i -lt 100; $i++)
 {
  $m='CheckAssumeCase100_' + $i;
  (Get-Content .\CheckAssumeCase100.cs) -Replace 'CheckAssumeCase100', $m | Set-Content .\CheckAssumeCase100_$i.cs
