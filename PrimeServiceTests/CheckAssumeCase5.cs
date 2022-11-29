e2d2ba868f08962f9379a9fd12640f29f6fab1a5
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-11-29 22:58:31 +0100

    Update CheckAssumeCase5.cs

--- PrimeServiceTests/CheckAssumeCase5.cs
+++ PrimeServiceTests/CheckAssumeCase5.cs
@@ -8,7 +8,7 @@ namespace Prime.UnitTests.Services
 {
     public class CheckAssumeCase5
     {
-        private string _appUrl;
+
         //public TestContext TestContext { get; set; }
 
         [Test]
@@ -25,7 +25,7 @@ namespace Prime.UnitTests.Services
         {
             Thread.Sleep(500);
 
-         Assert.AreEqual(TestContext.CurrentContext.Test.Properties.Get("webAppUrl"), 1);
+         Assert.AreEqual(TestContext.Parameters["webAppUrl"], 1);
 
 
            
